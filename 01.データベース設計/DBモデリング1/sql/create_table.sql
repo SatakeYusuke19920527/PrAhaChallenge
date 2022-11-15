48661438d53dd117e4170f978f3e7073f6149397
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-11-15 09:39:36 +0900

    sql作成中

--- 01.データベース設計/DBモデリング1/sql/create_table.sql
+++ 01.データベース設計/DBモデリング1/sql/create_table.sql
@@ -1,16 +1,20 @@
 -- CREATE DATABASE
-CREATE DATABASE IF NOT EXISTS sushi
+CREATE DATABASE IF NOT EXISTS sushi;
 
 -- CREATE TABLE
 CREATE TABLE IF NOT EXISTS sushi.OrderType (
   ot_no int AUTO_INCREMENT NOT NULL, 
   ot_name varchar(30) NOT NULL, 
+  created_at datetime default current_timestamp,
+  updated_at timestamp default current_timestamp on update current_timestamp,
   PRIMARY KEY (ot_no)
 );
 
 CREATE TABLE IF NOT EXISTS sushi.ProductType (
   pt_no int AUTO_INCREMENT NOT NULL, 
   pt_name varchar(30) NOT NULL, 
+  created_at datetime default current_timestamp,
+  updated_at timestamp default current_timestamp on update current_timestamp,
   PRIMARY KEY (pt_no)
 );
 
@@ -19,6 +23,8 @@ CREATE TABLE IF NOT EXISTS sushi.Product (
   p_name varchar(30) NOT NULL, 
   ot_no int NOT NULL,
   pt_no int NOT NULL,
+  created_at datetime default current_timestamp,
+  updated_at timestamp default current_timestamp on update current_timestamp,
   PRIMARY KEY (p_no),
   FOREIGN KEY fk_ot_no(ot_no) REFERENCES OrderType(ot_no),
   FOREIGN KEY fk_pt_no(pt_no) REFERENCES ProductType(pt_no)
@@ -27,6 +33,8 @@ CREATE TABLE IF NOT EXISTS sushi.Product (
 CREATE TABLE IF NOT EXISTS sushi.Price (
   p_no int AUTO_INCREMENT NOT NULL, 
   price int NOT NULL,
+  created_at datetime default current_timestamp,
+  updated_at timestamp default current_timestamp on update current_timestamp,
   FOREIGN KEY fk_p_no(p_no) REFERENCES Product(p_no)
 );
 
@@ -35,6 +43,8 @@ CREATE TABLE IF NOT EXISTS sushi.Customer (
   c_name varchar(30) NOT NULL, 
   phonenumber varchar(30) NOT NULL, 
   payment_flag int,
+  created_at datetime default current_timestamp,
+  updated_at timestamp default current_timestamp on update current_timestamp,
   PRIMARY KEY (c_no)
 );
 
@@ -45,6 +55,9 @@ CREATE TABLE IF NOT EXISTS sushi.OrderDetails (
   amount int NOT NULL,
   sabi_flag varchar(30) NOT NULL, 
   ricesize_flag varchar(30) NOT NULL, 
+  ricekind_flag varchar(30) NOT NULL, 
+  created_at datetime default current_timestamp,
+  updated_at timestamp default current_timestamp on update current_timestamp,
   PRIMARY KEY (od_no),
   FOREIGN KEY fk_c_no(c_no) REFERENCES Customer(c_no),
   FOREIGN KEY fk_p_no(p_no) REFERENCES Price(p_no)
@@ -53,7 +66,7 @@ CREATE TABLE IF NOT EXISTS sushi.OrderDetails (
 CREATE TABLE IF NOT EXISTS sushi.OrderHistory (
   od_no int NOT NULL, 
   order_date date NOT NULL,
+  created_at datetime default current_timestamp,
+  updated_at timestamp default current_timestamp on update current_timestamp,
   FOREIGN KEY fk_od_no(od_no) REFERENCES OrderDetails(od_no)
 );
-
--- INSERT INFOMATION
