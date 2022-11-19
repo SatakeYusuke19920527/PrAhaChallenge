7ec074fc3062aa1897a99d6da8b65365025bef25
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-11-19 21:33:44 +0900

    commit

--- 01.データベース設計/DBモデリング1/sql/monthly_aggregation.sql
+++ 01.データベース設計/DBモデリング1/sql/monthly_aggregation.sql
@@ -1,25 +1,23 @@
 -- 各月の集計を出すクエリ
-select 
-	 PT.p_name as 商品名,
-	sum(OD.amount) as 売り上げ個数
+select
+    sum(OD.amount*PE.price) as 支払い金額,
+    DATE_FORMAT(O.order_date, '%Y-%m') as 各月の集計金額
 from 
 	OrderDetails as OD
 inner join 
-	Order as O
+	sushi.Order as O
 on
-	OD.od_id = O.od_id	
+	OD.od_id = O.od_id
 inner join 
 	Product as PT
 on
-	OD.p_id = PT.p_id
+	OD.p_id = PT.p_id	
 inner join 
 	Price as PE
 on
 	PT.price_id = PE.price_id
-where
-	PT.pt_id = 5 and O.order_date BETWEEN '2022/09/01 00:00:00' AND '2022/09/30 23:59:59'
 group by
-	 PT.p_name
+  	O.order_date
 
 -- 11月に売れた商品の注文数量を算出するクエリ
 select 
